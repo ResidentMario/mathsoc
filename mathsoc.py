0b54abe195316845f4824de9f454d8a02d920211
ResidentMario <aleksey.bilogur@gmail.com>
2015-10-20 18:47:07 +0000

    Initial commit.

---
+++ mathsoc.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template('about.html')
+
+@app.route("/Board")
+def board_page():
+	return render_template('board.html')
+
+@app.route("/Contact")
+def contact_page():
+	return render_template('contact.html')
+
+@app.route("/Calendar")
+def calender_page():
+	return render_template('calendar.html')
+
+if __name__ == "__main__":
+    #app.run(debug=True)
+    app.run()
